bc922131750f0831378708bd856ab3803d0986d6
kafka <jjk5158@naver.com>
2022-05-15 19:10:57 +0900

    "2022/05/11"

--- src/test/java/hello/jdbc/connection/DBConnectionUtilTest.java
+++ src/test/java/hello/jdbc/connection/DBConnectionUtilTest.java
@@ -1,7 +1,7 @@
 package hello.jdbc.connection;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.sql.Connection;
